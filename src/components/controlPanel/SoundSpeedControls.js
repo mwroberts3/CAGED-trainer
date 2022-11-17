095e282446f1615a3ea8988823816ffcb8962de0
Mike <mwroberts89@gmail.com>
2022-11-17 09:41:00 -0500

    not the most elegant solution, but data up and running, speed wont be an issue sorting through the array

--- src/components/controlPanel/SoundSpeedControls.js
+++ src/components/controlPanel/SoundSpeedControls.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { FaVolumeMute, FaVolumeUp, FaPlay, FaPause } from 'react-icons/fa'
 
 const SoundSpeedControls = () => {
-  return (
-    <div>SoundSpeedControls</div>
-  )
+  return <>
+      <div className='sound-control option-setting'>
+        <FaVolumeUp />
+        <FaPlay />
+      </div>
+      <div className='speed-control option-setting'>
+        <label>Speed:</label>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+  </>
 }
 
 export default SoundSpeedControls
--- src/components/controlPanel/TrainingProtocol.js
+++ src/components/controlPanel/TrainingProtocol.js
@@ -1,9 +1,57 @@
 import React from 'react'
 
 const TrainingProtocol = () => {
-  return (
-    <div>TrainingProtocol</div>
-  )
+  return <>
+      <div className='position-range-control option-setting'>
+        <h3>Fret Range:</h3>
+        <label>Min:</label>
+        <select>
+          <option>open</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+          <option>13</option>
+          <option>14</option>
+          <option>15</option>
+        </select>
+        <label>Max:</label>
+        <select>
+          <option>open</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+          <option>13</option>
+          <option>14</option>
+          <option>15</option>
+        </select>
+      </div>
+      <div className='type-control option-setting'>
+        <label>Type:</label>
+        <select>
+          <option>chords</option>
+          <option>scales</option>
+          <option>mix</option>
+        </select>
+      </div>
+  </>
 }
 
 export default TrainingProtocol
