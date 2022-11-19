a9953e6e39b29f9e128d11c0bda057c30e6cc187
Mike <mwroberts89@gmail.com>
2022-11-19 09:17:53 -0500

    random training unit generation working for current unit

--- src/components/displayHUD/DisplayHUD.js
+++ src/components/displayHUD/DisplayHUD.js
@@ -5,15 +5,38 @@ import { useGlobalContext } from '../../context'
 import { data } from '../../data'
 
 const DisplayHUD = () => {
+  console.log(data);
+
   const { trainingParameters, speed, play } = useGlobalContext();
 
   const [trainingUnits, setTrainingUnits] = useState({})
 
   // will need to get and use the speed and play/pause bindings through context
-  const generateNextTrainingUnit = () => {   
+  const generateNextTrainingUnit = () => { 
+    let randomIndex = Math.random();
+    randomIndex = Math.floor(randomIndex * 79);
+    console.log(randomIndex);
+
+    let tempForm = Object.keys(data.chords[randomIndex])[0].substring(0,1);
+    
+    let tempPos = createPosDis();
+
+    let tempChord = Object.values(data.chords[randomIndex])[0];
+
     setTrainingUnits({prev: {form: 'prev form', position: 'prev pos', chord: 'prev chord'},
-    current: {form: Math.random(), position: 'current pos', chord: 'current chord'},
+    current: {form: `${tempForm} form`, position: `${tempPos} position`, chord: tempChord},
     next: {form: 'next form', position: 'next pos', chord: 'next chord'}});
+
+    function createPosDis() {
+      let pos = +Object.keys(data.chords[randomIndex])[0].substring(1);
+
+      if (pos === 0) return 'open';
+      if (pos === 1) return '1st';
+      if (pos === 2) return '2nd';
+      if (pos === 3) return '3rd';
+
+      return `${pos}th`;
+    }
   }
 
 
@@ -21,7 +44,7 @@ const DisplayHUD = () => {
   // generate first random training unit
   useEffect(() => {
     generateNextTrainingUnit();
-    
+
     setInterval(() => {
     console.log('next training unit')
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { AppProvider } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>
+  </>
 );
